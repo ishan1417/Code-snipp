bd66dfaaa3906be2f59a835e8c6c72d3b6d4d2ee
Ishan Gupta <ishu.stu@gmail.com>
2026-01-02 11:58:58 +0530

    4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
 import React from 'react'
-import Counter from './Questions/Counter'
-import Debounce from './Questions/Debounce'
-import Pagination from './Questions/Pagination'
-import Search from './Questions/Search'
-import Throttling from './Questions/Throttling'
-import ToDoList from './Questions/ToDoList'
-
 const App = () => {
   return (
     <>
